8060f451dfc99821e8cd40c704624dd183161b5f
aayush-aryan <log2aayusharyan@gmail.com>
2020-10-20 15:43:22 +0530

    [aayush]added. declaring a method for checking winning condition

--- src/org/gamblerSimulator/GamblingSimulator.java
+++ src/org/gamblerSimulator/GamblingSimulator.java
@@ -1,7 +1,32 @@
 package org.gamblerSimulator;
 
+import java.util.Random;
+
 public class GamblingSimulator {
-    double stack =100;
-    double bet =1;
+    double STACK =100;
+    double BET =1;
+    double WIN =0;
+    double LOOSE=0;
+
+    public void chekWinningLoose(){
+        Random random = new Random();
+        int randomNumber =  random.nextInt(2);
+        if(randomNumber==0){
+            WIN=BET;
+            System.out.println("gambler is going to win and Winning Amount"+WIN);
+
+        }else{
+            LOOSE=BET;
+            System.out.println("gambler is going to loose and Loosing amount"+BET);
+           
+        }
+
+    }
+
+    public static void main(String[] args) {
+        GamblingSimulator gamblingSimulator = new GamblingSimulator();
+        gamblingSimulator.chekWinningLoose();
+    }
+
 
 }
