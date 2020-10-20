90df97c1d8eb938932a6937307e9d54109b0ffe4
aayush-aryan <log2aayusharyan@gmail.com>
2020-10-20 16:31:40 +0530

    [aayush]added. refactor method and putting a condition for gambler win or loose 50% of his stack

--- src/org/gamblerSimulator/GamblingSimulator.java
+++ src/org/gamblerSimulator/GamblingSimulator.java
@@ -3,23 +3,28 @@ package org.gamblerSimulator;
 import java.util.Random;
 
 public class GamblingSimulator {
-    double STACK =100;
-    double BET =1;
-    double WIN =0;
-    double LOOSE=0;
+    public static final int  STACK =100;
+    public static final int BET =1;
+    public static int WIN =0;
+    public static int LOOSE=0;
 
     public void chekWinningLoose(){
-        Random random = new Random();
-        int randomNumber =  random.nextInt(2);
-        if(randomNumber==0){
-            WIN=BET;
-            System.out.println("gambler is going to win and Winning Amount"+WIN);
 
-        }else{
-            LOOSE=BET;
-            System.out.println("gambler is going to loose and Loosing amount"+BET);
-           
+        Random random = new Random();
+        int tempStack=STACK;
+        while (tempStack>50 && tempStack<150) {
+            int randomNumber = random.nextInt(2);
+            if (randomNumber == 0) {
+                tempStack=tempStack+BET;
+                System.out.println("gambler is going to win and Winning Amount" + WIN);
+
+            } else {
+                tempStack=tempStack-BET;
+                System.out.println("gambler is going to loose and Loosing amount" + BET);
+
+            }
         }
+        System.out.println("total stack :" + tempStack);
 
     }
 
